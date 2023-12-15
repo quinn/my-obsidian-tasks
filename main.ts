1c88db807ff3075e0f9adb0a2a194198491a74f7
quinn <q.shanahan@gmail.com>
2023-12-14 20:13:30 -0500

    more deno stuff

--- main.ts
+++ main.ts
@@ -1,95 +1,133 @@
-import { App, Editor, MarkdownFileInfo, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownFileInfo,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from 'obsidian'
+import { compose, filter, prop, sortBy } from 'https://esm.sh/rambda@8.6.0'
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	mySetting: string
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
 }
+const findDailyNotes = compose(
+	filter((f: TFile) => f.parent?.name === 'Daily'),
+	sortBy(prop('name')),
+)
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings | undefined;
+	settings?: MyPluginSettings
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings()
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			'dice',
+			'Sample Plugin',
+			(_evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice('Hello from Deno')
+			},
+		)
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('my-plugin-ribbon-class')
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const statusBarItemEl = this.addStatusBarItem()
+		statusBarItemEl.setText('Status Bar Text')
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
-				new SampleModal(this.app, 'Woah!').open();
-			}
-		});
+				new SampleModal(this.app, 'Woah!').open()
+			},
+		})
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
-				const cView = view as MarkdownView
-				const text = editor.getValue()
-				cView.editor.setValue(text)
-				new SampleModal(this.app, 'Done').open();
-			}
-		});
+			name: 'Fix daily notes links',
+			editorCallback: (
+				editor: Editor,
+				_view: MarkdownView | MarkdownFileInfo,
+			) => {
+				const dailyNotes = findDailyNotes(this.app.vault.getFiles())
+
+				const line = editor.getLine(1)
+
+				console.log(dailyNotes)
+				console.log(line)
+
+				// const cView = view as MarkdownView
+				// const text = editor.getValue()
+				// cView.app.fileManager.
+				// cView.editor.setValue(text)
+
+				new SampleModal(this.app, 'Done').open()
+			},
+		})
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView = this.app.workspace.getActiveViewOfType(
+					MarkdownView,
+				)
+
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app, 'Woah!').open();
+						new SampleModal(this.app, 'Woah!').open()
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					return true
 				}
-			}
-		});
+			},
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SampleSettingTab(this.app, this))
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			console.log('click', evt)
+		})
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
+		)
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
 	}
 }
 
@@ -97,45 +135,49 @@ class SampleModal extends Modal {
 	private text: string
 
 	constructor(app: App, text: string) {
-		super(app);
+		super(app)
 		this.text = text
 	}
 
 	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText(this.text);
+		const { contentEl } = this
+		contentEl.setText(this.text)
 	}
 
 	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
+		const { contentEl } = this
+		contentEl.empty()
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: MyPlugin
 
 	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		super(app, plugin)
+		this.plugin = plugin
 	}
 
 	display(): void {
-		const { containerEl } = this;
+		const { containerEl } = this
 
-		containerEl.empty();
+		containerEl.empty()
 
 		new Setting(containerEl)
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings?.mySetting ?? '')
-				.onChange(async (value) => {
-					if (this.plugin.settings) {
-						this.plugin.settings.mySetting = value;
-					}
-					await this.plugin.saveSettings();
-				}));
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings?.mySetting ?? '')
+					.onChange(async (value) => {
+						if (this.plugin.settings) {
+							this.plugin.settings.mySetting = value
+						} else {
+							throw new Error('settings is missing')
+						}
+						await this.plugin.saveSettings()
+					})
+			)
 	}
 }
